92b9abe81f95d3fa27de63f1fea103e93620d162
BarnetteME1 <BarnetteME1@gmail.com>
2017-09-17 17:33:43 -0400

    got initial view working

--- kijabi_site/kijabi_site/urls.py
+++ kijabi_site/kijabi_site/urls.py
@@ -15,12 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include,url
 from django.contrib import admin
-from kijabiapp import index
+from kijabiapp.views import index
 
 urlpatterns = [
+    url(r'^$',index,name='index'),
     url(r'^admin/', admin.site.urls),
-]
-
-urlpatterns = [
-    url(r'^$',index,name='index')
 ]
