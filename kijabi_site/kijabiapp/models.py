26831966f2461a6f00d553eff4deebbeaa70bcf1
BarnetteME1 <BarnetteME1@gmail.com>
2017-10-04 20:02:43 -0400

    added a models and worked some on HTML. Need to work on base.html

--- kijabi_site/kijabiapp/models.py
+++ kijabi_site/kijabiapp/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class culture_info(models.Model):
+    author = models.ForeignKey(User)
+    timestampe = models.DateTimeField(auto_now_add=True)
+    bacteria_type = models.CharField(max_length=10)
+    culture_results = models.CharField(max_length=3)
+    culture_site = models.CharField(max_length=25)
+    Diagnosis = models.TextField()
+    ward = models.CharField(max_length=25)
+    med_department = models.CharField(max_length=25)
+    device = models.CharField(max_length=1)
+    device_duration = models.IntegerField()
+    patient_number = models.IntegerField()
+    previous_admission = models.CharField(max_length=1)
+    previous_discharge_date = models.DateTimeField()
